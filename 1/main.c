60f738b801055eac25ff66752681f4ffaf39f3dc
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2015-09-27 01:28:31 +0300

    1 task completed

--- 1/main.c
+++ 1/main.c
@@ -1,69 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <linux/limits.h>
 #include <errno.h>
+
+#define HNDSHK_FIFO_NAME "0"
+#define TX_PID_STR_SIZE 16
+
+#define CLR_RED     "\x1b[31m"
+#define CLR_DEFAULT "\x1b[0m"
+
+extern int errno;
 
 int main(int argc, char *argv[])
 {
-    /*
-    if (argc < 2)
+    if (argc == 1) //Receiver mode
     {
-        printf("Arguments number too small.\n");
-        return 1;
-    }
-    execvp(argv[1], argv + 1);
-    printf("Exec error\n");
-    return 0;
-    */
+        //Handshake
+        errno = 0;
+        if (mkfifo(HNDSHK_FIFO_NAME, 0666) && (errno != EEXIST))
+        {
+            printf("FIFO '%s' creation error.\n", HNDSHK_FIFO_NAME);
+            return -1;
+        }
 
-    if (argc != 2)
-    {
-        printf("Invalid arguments.\n");
-        return 1;
-    }
+        int out_hndshk_fifo_fd = open(HNDSHK_FIFO_NAME, O_RDONLY);
+        if (out_hndshk_fifo_fd == -1)
+        {
+            printf("FIFO opening error.\n");
+            return -1;
+        }
 
-    char *endptr = NULL;
-    errno = 0;
-    long int num = strtol(argv[1], &endptr, 10);
+        char tx_pid_str[TX_PID_STR_SIZE] = {};
+        read(out_hndshk_fifo_fd, tx_pid_str, TX_PID_STR_SIZE);
+        if (tx_pid_str[0] == '\0')
+        {
+            printf("%sEOF received.%s\n", CLR_RED, CLR_DEFAULT);
+            return -1;
+        }
+        //printf("{%s}\n", tx_pid_str);
 
-    if (errno == ERANGE)
-    {
-        printf("Number of process is out of range.\n");
-        return 1;
+        //Reception
+        errno = 0;
+        if (mkfifo(tx_pid_str, 0666) && (errno != EEXIST))
+        {
+            printf("FIFO '%s' creation error.\n", tx_pid_str);
+            return -1;
+        }
+
+        int out_fifo_fd = open(tx_pid_str, O_RDONLY);
+        if (out_fifo_fd == -1)
+        {
+            printf("FIFO opening error.\n");
+            return -1;
+        }
+
+        int read_num = 1;
+        char out_buffer[PIPE_BUF];
+        while (read_num > 0)
+        {
+            read_num = read(out_fifo_fd, out_buffer, PIPE_BUF);
+            write(1, out_buffer, read_num);
+        }
+
+        close(out_fifo_fd);
+        close(out_hndshk_fifo_fd);
+        unlink(tx_pid_str);
     }
-    else if (*endptr != '\0')
-    {
-        printf("Invalid process number.\n");
-        return 1;
+    else if (argc == 2) //Tranceiver mode
+    {   
+        //Handshake
+        errno = 0;
+        if (mkfifo(HNDSHK_FIFO_NAME, 0666) && (errno != EEXIST))
+        {
+            printf("FIFO '%s' creation error.\n", HNDSHK_FIFO_NAME);
+            return -1;
+        }
+
+        int in_hndshk_fifo_fd = open(HNDSHK_FIFO_NAME, O_WRONLY);
+        if (in_hndshk_fifo_fd == -1)
+        {
+            printf("FIFO opening error.\n");
+            return -1;
+        }
+
+        char tx_pid_str[TX_PID_STR_SIZE] = {};
+        sprintf(tx_pid_str, "P%d", getpid());
+        //printf("{%s}\n", tx_pid_str);
+        write(in_hndshk_fifo_fd, tx_pid_str, TX_PID_STR_SIZE);
+        
+        //Transfer
+        int file_fd = open(argv[1], O_RDONLY);
+        if (file_fd == -1)
+        {
+            printf("File opening error.\n");
+            return -1;
+        }
+
+        errno = 0;
+        if (mkfifo(tx_pid_str, 0666) && (errno != EEXIST))
+        {
+            printf("FIFO '%s' creation error.\n", tx_pid_str);
+            close(file_fd);
+            return -1;
+        }
+
+        int in_fifo_fd = open(tx_pid_str, O_WRONLY);
+        if (in_fifo_fd == -1)
+        {
+            printf("FIFO opening error.\n");
+            close(file_fd);
+            return -1;
+        }
+
+        int read_num = 1;
+        char in_buffer[PIPE_BUF];
+        while (read_num > 0)
+        {
+            read_num = read(file_fd, in_buffer, PIPE_BUF);
+            write(in_fifo_fd, in_buffer, read_num);
+        }
+
+        close(file_fd);
+        close(in_fifo_fd);
+        close(in_hndshk_fifo_fd);
     }
     else
     {
-        if (num < 0)
-        {
-            printf("Negative process number.\n");
-            return 1;
-        }
-    }
-
-    printf("num = %ld\n", num);
-    int pid = -2;
-    int status;
-
-    for (int i = 0; i < num; ++i)
-    {
-        pid = fork();
-        switch(pid) 
-        {
-        case -1: 
-            printf("Fork failed.\n");
-            exit(-1);
-        case 0: 
-            printf("Child #%d of %ld with %ld\n", i, getppid(), getpid());
-            exit(0);
-        default: 
-            break;
-        }
-        wait(&status);
+        printf("Invalid arguments.\n");
+        return -1;
     }
 
     return 0;
